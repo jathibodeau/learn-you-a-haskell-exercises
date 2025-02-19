a775129c5d337543ff0d5475b7230c929e819f36
John Thibodeau <jthibo@gmail.com>
2025-02-19 00:16:08 -0400

    Solutions for 12.

--- 12-a-fistful-of-monads.hs
+++ 12-a-fistful-of-monads.hs
@@ -5,27 +5,46 @@
  -   Success, which takes that parameter and
  -   Fail String, which represents a failure, and a reason for that failure
  -}
---data Validation a 
+data Validation a
+  = Success a
+  | Fail String
+  deriving (Eq, Show)
 
 -- Make the Validation a Monad
+instance Functor Validation where
+  fmap f (Success a) = Success $ f a
+  fmap _ (Fail x) = Fail x
 
+instance Applicative Validation where
+  pure = Success
+  (<*>) (Success f) = fmap f
+  (<*>) (Fail x) = const $ Fail x
+
+instance Monad Validation where
+  return = pure
+  (>>=) (Success x) f = f x
+  (>>=) (Fail x) _ = Fail x
 
 {-
  - Create a function, positiveCheck, which takes a number and returns a successful Validation if it's positive, 
  - and a failed Validation with a String message if not.
  -}
---positiveCheck :: (Num a, Ord a) => a -> Validation a
---positiveCheck x = undefined
+positiveCheck :: (Num a, Ord a) => a -> Validation a
+positiveCheck x
+  | signum x == 1 = Success x
+  | otherwise = Fail "Not a positive number!"
 
 {-
  - Create a function, evenCheck, which returns a successful Validation if it's even,
  - and a failed Validation with a string message if it's odd
  -}
---evenCheck :: (Integral a)  =>  a -> Validation a
---evenCheck x = undefined
+evenCheck :: (Integral a)  =>  a -> Validation a
+evenCheck x
+  | even x = Success x
+  | otherwise = Fail "Not an even number!"
 
 {-
  - Write a function which uses positiveCheck and evenCheck to make sure a number is both positive and even
  -}
---positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
---positiveAndEvenCheck x = undefined
+positiveAndEvenCheck :: (Num a, Ord a, Integral a) => a -> Validation a
+positiveAndEvenCheck x = positiveCheck x >>= evenCheck
