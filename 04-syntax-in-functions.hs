1e39ac485e55a73b21917fb0460046d74882fd75
John Thibodeau <jthibo@gmail.com>
2025-02-23 00:02:40 -0400

    Solutions for 04.

--- 04-syntax-in-functions.hs
+++ 04-syntax-in-functions.hs
@@ -1,8 +1,8 @@
 -- This function should print a single digit number as English text, or "unknown" if it's out of the range 0-9
+import Data.List ((!?))
+import Data.Maybe (fromMaybe)
 englishDigit :: Int -> String
-englishDigit x
-  | x >= 0 && x <=9 = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] !! x
-  | otherwise = "unknown"
+englishDigit = fromMaybe "unknown" . (!?) ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 -- given a tuple, divide fst by snd, using pattern matching. 
 -- it should return undefined for division by zero
